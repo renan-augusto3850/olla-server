b817f93a5ccc606efc542ec258f9499f0eee7d71
Renan Augusto <renancandido82@outlook.com>
2024-07-20 20:42:42 -0300

    TRY SEND TO NETLIFY SPECS.

--- functions/index.js
+++ functions/index.js
@@ -13,52 +13,11 @@ const peerServer = ExpressPeerServer(server, {
     debug: true,
 });
 
-app.use('/peerjs', peerServer);
+app.use('/olla', peerServer);
 app.get('/', (req, res) => {
     res.send('<h1>Olla Server!</h1>');
 });
 
-exports.handler = (event, context, callback) => {
-    server.listen(9000, () => {
-        console.log('PeerJS server is running on port 9000');
-    });
-
-    // Simula a solicitação HTTP do Netlify para o Express
-    const req = {
-        method: event.httpMethod,
-        url: event.path,
-        headers: event.headers,
-        body: event.body,
-        query: event.queryStringParameters
-    };
-
-    const res = {
-        setHeader: (name, value) => {
-            callback(null, {
-                statusCode: 200,
-                headers: {
-                    [name]: value
-                },
-                body: ''
-            });
-        },
-        end: (body) => {
-            callback(null, {
-                statusCode: 200,
-                body: body
-            });
-        },
-        writeHead: (statusCode, headers) => {
-            callback(null, {
-                statusCode: statusCode,
-                headers: headers,
-                body: ''
-            });
-        },
-        write: (body) => {
-            res.body = body;
-        }
-    };
-
-    app(req, res);
-};
+server.listen(9000, () => {
+    console.log('PeerJS server is running on port 3000');
+});
