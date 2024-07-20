d5be0ab8f1761452115bc76a8c046c70c41c8cef
Renan Augusto <renancandido82@outlook.com>
2024-07-20 20:48:08 -0300

    TRY SEND TO NETLIFY SPECS.

--- functions/index.js
+++ functions/index.js
@@ -9,15 +9,56 @@ const server = require('http').createServer(app);
 app.use(cors());
 
 const peerServer = ExpressPeerServer(server, {
-    path: '/peerjs',
-    debug: true,
+  path: '/peerjs',
+  debug: true,
 });
 
-app.use('/olla', peerServer);
+app.use('/peerjs', peerServer);
 app.get('/', (req, res) => {
-    res.send('<h1>Olla Server!</h1>');
+  res.send('<h1>Olla Server!</h1>');
 });
 
 server.listen(9000, () => {
-    console.log('PeerJS server is running on port 9000');
+  console.log('PeerJS server is running on port 9000');
 });
+
+exports.handler = async (event, context) => {
+  // Simula a solicitação HTTP do Netlify para o Express
+  const req = {
+    method: event.httpMethod,
+    url: event.path,
+    headers: event.headers,
+    body: event.body,
+    query: event.queryStringParameters
+  };
+
+  const res = {
+    setHeader: (name, value) => {
+      callback(null, {
+        statusCode: 200,
+        headers: {
+          [name]: value
+        },
+        body: ''
+      });
+    },
+    end: (body) => {
+      callback(null, {
+        statusCode: 200,
+        body: body
+      });
+    },
+    writeHead: (statusCode, headers) => {
+      callback(null, {
+        statusCode: statusCode,
+        headers: headers,
+        body: ''
+      });
+    },
+    write: (body) => {
+      res.body = body;
+    }
+  };
+
+  app(req, res);
+};
